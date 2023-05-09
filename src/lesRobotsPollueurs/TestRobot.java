8cdea36b527907f7d4777e1960a3fef771784c8d
Medovsky <ahmed123elbasri@gmail.com>
2023-05-09 09:54:07 +0100

    4th updated files

--- src/lesRobotsPollueurs/TestRobot.java
+++ src/lesRobotsPollueurs/TestRobot.java
@@ -13,6 +13,29 @@ public class TestRobot {
 		System.out.println(m.estSale(2, 3));
 		System.out.println(m.nbrPapierGras());
 		System.out.println(m);
+//		// TODO Auto-generated method stub
+//		Monde m=new Monde(10,10);
+//		
+//		/*PollueurTousDroit pl=new PollueurTousDroit(m);
+//		pl.Parcourir();*/
+//		//pl.Polluer();
+//		PollueurTousDroit ps=new PollueurTousDroit(m);
+//		ps.Parcourir();
+//	
+//	/*	
+//		if(m.estSale(0, 0)) {
+//			System.out.println("sale");
+//		}
+//		else {
+//			System.out.println("n'est pas sale");
+//		}
+//		*/
+//		NettoyeurDistrait net=new NettoyeurDistrait(m);
+//		net.Parcourir();
+//		
+//		
+//		System.out.println(m);
+
 
 	}
 
